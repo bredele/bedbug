e9a57b3c74530f846d82ddd476d993d3c8948eac
bredele <olivier.wietrich@gmail.com>
2014-04-01 10:09:29 -0600

    add plugin test

---
+++ index.js
@@ -0,0 +1,17 @@
+
+/**
+ * Expose 'debug'
+ */
+
+module.exports = function(ctx) {
+	ctx.debug = debug;
+};
+
+
+/**
+ * debug constructor.
+ * @api public
+ */
+
+function debug() {
+}
---
+++ test/debug.js
@@ -0,0 +1,18 @@
+var debug = require('..');
+var assert = require('assert');
+var plumby = require('plumby');
+
+describe('debug', function() {
+	
+	var app;
+	beforeEach(function() {
+		app = plumby();
+		app.use(debug);
+	});
+
+	it('should add a debug handler', function() {
+		assert(app.debug);
+	});
+
+	
+});
